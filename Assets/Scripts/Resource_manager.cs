3d04d51a5dec9cb2421d79a4f0a4d3d3c64ae324
Xin Lu <bruyereseb@gmail.com>
2018-01-17 15:37:32 +0000

    downloading feature finished

---
+++ Assets/Scripts/Resource_manager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class resource_manager : MonoBehaviour {
+
+    private WWW www;                     //request
+
+
+    private string[] URL;   // array of URLs of the image
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //do something here to initialise the array of all URLs of images.
+
+
+
+        StartCoroutine(Download());
+
+    }
+
+
+
+    IEnumerator Download()
+    {
+        Texture2D texture2D;         //downloaded images
+        string filePath;             //path of downloaded images
+        foreach (string current_url in URL) {
+            //download
+            www = new WWW(current_url);
+            yield return www;
+
+            //save texture to file after downloading
+            texture2D = www.texture;
+            byte[] bytes = texture2D.EncodeToPNG();
+            filePath = Application.dataPath + "/resources/" + current_url;           //configure path of texture.
+            File.WriteAllBytes(filePath, bytes);
+        }
+    }
+
+}
