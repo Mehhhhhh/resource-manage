9220af118b64b09b266210eb739f8c09a0b854bf
Xin Lu <bruyereseb@gmail.com>
2018-01-17 16:26:31 +0000

    bugs fixed and added small test

--- Assets/Scripts/Resource_manager.cs
+++ Assets/Scripts/Resource_manager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class resource_manager : MonoBehaviour {
+public class Resource_manager : MonoBehaviour {
+    public GameObject cube;
 
     private WWW www;                     //request
 
@@ -17,7 +18,8 @@ public class resource_manager : MonoBehaviour {
     {
         //do something here to initialise the array of all URLs of images.
 
-
+        URL = new string[1];
+        URL[0] = "https://forum.unity.com/attachments/unity3d-png.177351/";
 
         StartCoroutine(Download());
 
@@ -37,9 +39,23 @@ public class resource_manager : MonoBehaviour {
             //save texture to file after downloading
             texture2D = www.texture;
             byte[] bytes = texture2D.EncodeToPNG();
-            filePath = Application.dataPath + "/resources/" + current_url;           //configure path of texture.
-            File.WriteAllBytes(filePath, bytes);
+            filePath = Application.dataPath + "/Resources/a";           //configure path of texture.
+            FileStream filestream = new FileStream(filePath, FileMode.Create,FileAccess.ReadWrite);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                filestream.WriteByte(bytes[i]);
+            }
         }
+        cube.GetComponent<Renderer>().material.mainTexture = Resources.Load("a") as Texture;
+
+
+        /*
+         * www = new WWW(Application.dataPath + "/Resources/a.png");
+        yield return www;
+        Texture2D tex = new Texture2D(4, 4);
+        www.LoadImageIntoTexture(tex);
+        cube.GetComponent<Renderer>().material.mainTexture = tex;
+        */
     }
 
 }
