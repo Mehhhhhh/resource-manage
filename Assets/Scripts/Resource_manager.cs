66ce8b9aadb979aec11918d00c1ef1a18a8d38bd
Xin Lu <bruyereseb@gmail.com>
2018-01-18 19:10:35 +0000

    added saving loading functions

--- Assets/Scripts/Resource_manager.cs
+++ Assets/Scripts/Resource_manager.cs
@@ -3,52 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class Resource_manager : MonoBehaviour {
-    public GameObject cube;
+public class Resource_manager : MonoBehaviour
+{
+    public GameObject obj;
 
-    private WWW www;                     //request
+    private int counter;
+    private string folderpath;
+    private string[] URL;   // array of URLs of the images
+    private ArrayList UniqueURL ;
 
-
-    private string[] URL;   // array of URLs of the image
 
 
 
     // Use this for initialization
     void Start()
     {
+        UniqueURL = new ArrayList();
+        counter = 0;
         //do something here to initialise the array of all URLs of images.
 
         URL = new string[1];
-        URL[0] = "https://forum.unity.com/attachments/unity3d-png.177351/";
-
-        StartCoroutine(Download());
+        URL[0] = "https://openclipart.org/image/300px/svg_to_png/14546/tonyk-Gnu-Knight.png&disposition=attachment";
+        folderpath = Application.persistentDataPath + "/";
+      
 
     }
 
 
 
-    IEnumerator Download()
+    void DownloadImages()
     {
-        Texture2D texture2D;         //downloaded images
-        string filePath;             //path of downloaded images
-        foreach (string current_url in URL) {
-            //download
-            www = new WWW(current_url);
-            yield return www;
-
-            //save texture to file after downloading
-            texture2D = www.texture;
-            byte[] bytes = texture2D.EncodeToPNG();
-            filePath = Application.dataPath + "/Resources/a";           //configure path of texture.
-            FileStream filestream = new FileStream(filePath, FileMode.Create,FileAccess.ReadWrite);
-            for (int i = 0; i < bytes.Length; i++)
+        foreach (string current_url in URL)
+        {
+            if (!Is_exist(current_url))
             {
-                filestream.WriteByte(bytes[i]);
+                StartCoroutine(DownloadSingleImage(current_url));
+               
+               
             }
         }
-        cube.GetComponent<Renderer>().material.mainTexture = Resources.Load("a") as Texture;
-
-
         /*
          * www = new WWW(Application.dataPath + "/Resources/a.png");
         yield return www;
@@ -56,6 +49,69 @@ public class Resource_manager : MonoBehaviour {
         www.LoadImageIntoTexture(tex);
         cube.GetComponent<Renderer>().material.mainTexture = tex;
         */
+    }
+
+
+    IEnumerator DownloadSingleImage(string url) {
+        string filePath  = folderpath + url;          //path of downloaded images
+        WWW www = new WWW(url);
+        yield return www;
+        counter++;
+        UniqueURL.Add(url);
+        SaveTexture(www, url);
+    }
+
+    //save texture to file after downloading
+    void SaveTexture(WWW www, string name) {
+            Texture2D texture2D = www.texture;     //downloaded images 
+            byte[] bytes = texture2D.EncodeToPNG();
+            string filePath = folderpath + "a";           //configure path of texture.
+        Debug.Log(filePath);
+            FileStream filestream = new FileStream(filePath, FileMode.Create);
+            filestream.Write(bytes, 0, bytes.Length);
+            filestream.Close();
+    }
+
+    void LoadImage(string url) {
+        WWW www = new WWW(folderpath+"a");
+        Texture2D tex = new Texture2D(4,4);
+        www.LoadImageIntoTexture(tex);
+        obj.GetComponent<Renderer>().material.mainTexture = tex;
+    }
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(240, 100, 150, 25), "LoadTexture"))
+        {
+
+            LoadImage("a");
+            Debug.Log("Test");
+
+        }
+
+        if (GUI.Button(new Rect(240, 150, 150, 25), "Download Texture"))
+        {
+
+            DownloadImages();
+            Debug.Log("Test");
+
+        }
+    }
+
+    public int getNumberOfImages() {
+        return counter;
+    }
+
+
+    private bool Is_exist(string url) {
+        if (UniqueURL.Contains(url))
+        {
+            return true;
+        }
+        else {
+            return false;
+           }
     }
 
 }
