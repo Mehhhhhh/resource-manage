8ef2ba018e829f9efb955b98a521bc52b3a43c5f
Xin Lu <bruyereseb@gmail.com>
2018-01-19 16:19:26 +0000

    fixed

--- Assets/Scripts/Resource_manager.cs
+++ Assets/Scripts/Resource_manager.cs
@@ -3,35 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Threading;
+using System;
 
 public class Resource_manager : MonoBehaviour
 {
-    public GameObject obj;
-
-    private int counter;
-    private string folderpath;
-    private string[] URL;   // array of URLs of the images
-    private ArrayList UniqueURL ;
-
+    private int counter;                 //number of images
+    private string folderpath;            
+    private string[] URL;              // array of URLs 
+    private Dictionary<string, int> UniqueURL;          // all the unique urls and the numbers of their corresponding image
+    private const int NumberOfUrls = 4;              //number of the given URLs 
+    const int NUMBEROFTHREADS = 5;
 
 
 
     // Use this for initialization
     void Start()
     {
-        UniqueURL = new ArrayList();
+        //initialization
+        UniqueURL = new Dictionary<string, int>();
         counter = 0;
         //do something here to initialise the array of all URLs of images.
-
-        URL = new string[1];
+        URL = new string[NumberOfUrls];
         URL[0] = "https://openclipart.org/image/300px/svg_to_png/14546/tonyk-Gnu-Knight.png&disposition=attachment";
-        folderpath = Application.persistentDataPath + "/";
+        URL[1] = "https://static.wixstatic.com/media/2cd43b_9e40d284910240479b94e37f071f702a~mv2.png/v1/fill/w_196,h_177,al_c,usm_0.66_1.00_0.01/2cd43b_9e40d284910240479b94e37f071f702a~mv2.png";
+        URL[2] = "http://www.pngmart.com/files/4/Nature-PNG-Free-Download.png";
+        URL[3] = "http://www.pngmart.com/files/4/Nature-PNG-Free-Download.png";
+        folderpath = Application.persistentDataPath + "/texture";
       
 
     }
 
 
-
+    /*
+     * Download images from the list of given URLs, if the URL is already existed, then load the coresponding image instead of downloading.
+     */
     void DownloadImages()
     {
         foreach (string current_url in URL)
@@ -39,74 +44,92 @@ public class Resource_manager : MonoBehaviour
             if (!Is_exist(current_url))
             {
                 StartCoroutine(DownloadSingleImage(current_url));
-               
-               
+
+
+            }
+            else {
+                LoadImage(UniqueURL[current_url]);
             }
         }
-        /*
-         * www = new WWW(Application.dataPath + "/Resources/a.png");
-        yield return www;
-        Texture2D tex = new Texture2D(4, 4);
-        www.LoadImageIntoTexture(tex);
-        cube.GetComponent<Renderer>().material.mainTexture = tex;
-        */
     }
 
 
+    /*
+     * download the image with given Url, then save the texture on the device
+     */
     IEnumerator DownloadSingleImage(string url) {
-        string filePath  = folderpath + url;          //path of downloaded images
+        counter++;
+        UniqueURL.Add(url, counter);
+        string filePath  = folderpath + url;          
         WWW www = new WWW(url);
         yield return www;
-        counter++;
-        UniqueURL.Add(url);
         SaveTexture(www, url);
     }
 
-    //save texture to file after downloading
+    /*
+     * save texture to file after downloading
+     */
     void SaveTexture(WWW www, string name) {
-            Texture2D texture2D = www.texture;     //downloaded images 
+            Texture2D texture2D = www.texture;     
             byte[] bytes = texture2D.EncodeToPNG();
-            string filePath = folderpath + "a";           //configure path of texture.
-        Debug.Log(filePath);
+            string filePath = folderpath + UniqueURL[name];           //configure path of texture.
+            Debug.Log(filePath);
             FileStream filestream = new FileStream(filePath, FileMode.Create);
             filestream.Write(bytes, 0, bytes.Length);
             filestream.Close();
     }
 
-    void LoadImage(string url) {
-        WWW www = new WWW("file:///"+ folderpath +"a");
-        Texture2D tex = new Texture2D(1,1);
-        www.LoadImageIntoTexture(tex);
-        obj.GetComponent<Renderer>().material.mainTexture = tex;
-        Debug.Log(tex);
+
+    /*
+     * load the selected image and set it as the texture of a generated gameobject, can be seen from game view.
+     */
+    void LoadImage(int n) {
+        if (File.Exists(folderpath + n.ToString()))
+        {
+            byte[] bytes = File.ReadAllBytes(folderpath + n.ToString());
+            Texture2D tex = new Texture2D(1, 1);
+            tex.LoadImage(bytes);
+
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = new Vector3(n, n, 0);
+            sphere.GetComponent<Renderer>().material.mainTexture = tex;
+            Debug.Log("loaded");
+        }
+        else { }
+        Debug.Log("not saved yet");
+
+       
     }
 
-
+    /*
+     * buttons to do the downloading and loading operations.
+     */ 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(240, 100, 150, 25), "LoadTexture"))
+        if (GUI.Button(new Rect(240, 150, 150, 25), "LoadTexture"))
         {
-
-            LoadImage("a");
+            for (int i = 1; i <= counter; i++)
+            {
+                LoadImage(i);
+                Debug.Log("loading" + i);
+            }
 
         }
 
-        if (GUI.Button(new Rect(240, 150, 150, 25), "Download Texture"))
+        if (GUI.Button(new Rect(240, 100, 150, 25), "Start download textures"))
         {
 
             DownloadImages();
-            Debug.Log("Test");
+            Debug.Log("download");
 
         }
     }
 
-    public int getNumberOfImages() {
-        return counter;
-    }
-
-
+    /*
+     * function used to detect if an Url has already been used to download image
+     */ 
     private bool Is_exist(string url) {
-        if (UniqueURL.Contains(url))
+        if (UniqueURL.ContainsKey(url))
         {
             return true;
         }
@@ -114,5 +137,8 @@ public class Resource_manager : MonoBehaviour
             return false;
            }
     }
+
+
+
 
 }
